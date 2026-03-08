576c3cb437a960f3f7674d72332a21e545c214be
Joseph Wasden <jpwasden@gmail.com>
2026-03-08 10:15:08 +0000

    Updated example script

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 mod keys;
 
 use keys::{KeyPair, KeyError};
+use crate::keys::public::Public;
 
 fn main() {
     match run_example() {
@@ -12,6 +13,9 @@ fn main() {
 fn run_example() -> Result<(), KeyError> {
     let bob = KeyPair::generate()?;
     let alice = KeyPair::generate()?;
+
+    let bob_pub = bob.public.to_b64();
+    let bob_pub_obj = Public::from_b64(&bob_pub)?;
 
     let plaintext = "Hello Bob! This is a secret message protected by post-quantum cryptography!";
 
