af2e98327f30445e8ca22326d000739d9a65c469
Joe Wasden <jpwaden93@gmail.com>
2026-04-08 16:47:08 +0100

    Added unique to id and username columns

--- migrations/20260407150346_init.up.sql
+++ migrations/20260407150346_init.up.sql
@@ -1,7 +1,7 @@
 -- Add up migration script here
 
 CREATE TABLE IF NOT EXISTS authentication (
-    id UUID DEFAULT gen_random_uuid(),
-    username TEXT NOT NULL,
+    id UUID UNIQUE DEFAULT gen_random_uuid(),
+    username TEXT UNIQUE NOT NULL,
     pw_hash TEXT NOT NULL
 );
