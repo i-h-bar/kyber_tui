cc533c072fb01e72ec944038dcbea1feb7278f30
Joe Wasden <jpwaden93@gmail.com>
2026-04-12 21:01:39 +0100

    Updated user creation query

--- crates/tui/src/main.rs
+++ crates/tui/src/main.rs
@@ -7,51 +7,36 @@ use contracts::token::PreAuthToken;
 use dotenv::dotenv;
 use pqcrypto::keys::pair::KeyPair;
 use std::env;
+use crate::api::ApiSession;
 
 #[tokio::main]
 async fn main() {
     dotenv().ok();
 
-    let key_pair = KeyPair::generate().unwrap();
+    let session = ApiSession::handshake().await.unwrap();
+    session.create_new_user("Other Username".to_string(), env::var("PASSWORD").unwrap()).await.unwrap();
 
-    let request = HandshakeRequest {
-        pub_key: key_pair.public.clone(),
-    };
-    let client = reqwest::Client::new();
 
-    let response: HandshakeResponse = client
-        .post("http://localhost:3000/handshake")
-        .json(&request)
-        .send()
-        .await
-        .unwrap()
-        .json()
-        .await
-        .unwrap();
-
-    let server_pub_key = response.public_key;
-    let token: PreAuthToken = key_pair.decrypt(&response.token, &server_pub_key).unwrap();
-
-    let new_user = AuthRequest {
-        username: "Other Username".to_string(),
-        password: env::var("PASSWORD").unwrap(),
-    };
-
-    println!("{}", token.session_id);
-    let new_user_request = GenericRequest {
-        session_id: token.session_id,
-        body: key_pair.encrypt(&new_user, &server_pub_key).unwrap(),
-        token: response.token,
-    };
-
-    let new_user_response = client
-        .post("http://localhost:3000/auth")
-        .json(&new_user_request)
-        .send()
-        .await
-        .unwrap();
-
-    println!("Response: {:?}", &new_user_response);
+    // let new_user = AuthRequest {
+    //     username: "Other Username".to_string(),
+    //     password: env::var("PASSWORD").unwrap(),
+    // };
+    //
+    // println!("{}", token.session_id);
+    // let new_user_request = GenericRequest {
+    //     session_id: token.session_id,
+    //     body: key_pair.encrypt(&new_user, &server_pub_key).unwrap(),
+    //     token: response.token,
+    // };
+    //
+    // let new_user_response = client
+    //     .post("http://localhost:3000/auth")
+    //     .json(&new_user_request)
+    //     .send()
+    //     .await
+    //     .unwrap();
+    //
+    // println!("Response: {:?}", &new_user_response);
 
     // let new_user_response = new_user_response.json::<GenericResponse>().await.unwrap();
 }
